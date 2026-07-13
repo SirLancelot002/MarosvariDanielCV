a04610694503f175409f677c7429379bf0fa35e3
SirLancelot002 <marosvariakiraly@gmail.com>
2026-07-13 15:01:07 +0200

    First animation
    For checking things on Github, how they work

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import TextType from './modules/TextType';
 import './App.css'
 
 function App() {
   return (
-    <>
-      
+    <><h1>
+      <TextType 
+        text={["Marosvári Dániel CV site", "Look around!", "Interactive and responsive"]}
+        typingSpeed={75}
+        pauseDuration={2500}
+        showCursor
+        cursorCharacter="_"
+        deletingSpeed={50}
+        variableSpeed={{ min: 60, max: 120 }}
+        cursorBlinkDuration={0.5}
+      /></h1>
     </>
   )
 }

601dbb9fd6d973bb7bcec55c716cd14927544794
SirLancelot002 <marosvariakiraly@gmail.com>
2026-07-17 16:17:56 +0200

    PillNav
    The Basic PillNav. This will need more improvements in the future, but first let's finish this page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import TextType from './modules/TextType';
+import PillNav from './modules/PillNav';
 import { useTranslation } from 'react-i18next';
+import logo from './assets/vscode.svg';
 import './App.css'
 
 function App() {
   const { t } = useTranslation();
 
   return (
-    <><h1>
-      <TextType 
+    <>
+      <main className="hero-section">
+      <TextType
+        as="h1"
         text={[t("hero.greeting"), t("hero.look"), t("hero.description")]}
         typingSpeed={75}
         pauseDuration={2500}
@@ -16,7 +20,25 @@ function App() {
         deletingSpeed={50}
         variableSpeed={{ min: 60, max: 120 }}
         cursorBlinkDuration={0.5}
-      /></h1>
+      /></main>
+      <PillNav
+        logo={logo}
+        logoAlt="Company Logo"
+        items={[
+          { label: 'Main', href: 'MarosvariDanielCV/' },
+          { label: 'Time Line', href: 'MarosvariDanielCV/timeline' },
+          { label: 'Studies', href: 'MarosvariDanielCV/studies' },
+          { label: 'Projects', href: 'MarosvariDanielCV/projects' }
+        ]}
+        activeHref="/"
+        className="custom-nav"
+        ease="power2.easeOut"
+        baseColor="#000000"
+        pillColor="#ffffff"
+        hoveredPillTextColor="#ffffff"
+        pillTextColor="#000000"
+        initialLoadAnimation
+      />
     </>
   )
 }

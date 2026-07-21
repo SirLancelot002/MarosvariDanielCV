8437c7c5c8a59cdc2a33354993d42d557bc230e8
SirLancelot002 <marosvariakiraly@gmail.com>
2026-07-21 15:53:30 +0200

    Lightning Background Perfect.

--- src/App.tsx
+++ src/App.tsx
@@ -23,67 +23,69 @@ function App() {
 
   return (
     <>
-      <div className="lightning-bg">
-        <Lightning
-          hue={260}
-          xOffset={0}
-          speed={1}
-          intensity={1}
-          size={1}
-        />
-      </div>
-      <main className="hero-section">
-        <TextType
-          as="h1"
-          text={[t("hero.greeting"), t("hero.look"), t("hero.description")]}
-          typingSpeed={75}
-          pauseDuration={2500}
-          showCursor
-          cursorCharacter="_"
-          deletingSpeed={50}
-          variableSpeed={{ min: 60, max: 120 }}
-          cursorBlinkDuration={0.5}
-        /></main>
-
-      <PillNav
-        logo={logo}
-        logoAlt="Company Logo"
-        items={[
-          { label: t("nav.main"), href: 'MarosvariDanielCV/' },
-          { label: t("nav.time"), href: 'MarosvariDanielCV/timeline' },
-          { label: t("nav.studies"), href: 'MarosvariDanielCV/studies' },
-          { label: t("nav.projects"), href: 'MarosvariDanielCV/projects' }
-        ]}
-        activeHref="/"
-        className="custom-nav"
-        ease="power2.easeOut"
-        baseColor="#000000"
-        pillColor="#ffffff"
-        hoveredPillTextColor="#ffffff"
-        pillTextColor="#000000"
-        initialLoadAnimation
-      />
-
-      <div className="profile-card-outer">
-        <div className="profile-card-inner">
-          <ProfileCard
-            name="Marosvári Dániel"
-            title={t("profileCard.job")}
-            handle="SirLancelot002"
-            status={t("profileCard.online")}
-            contactText={t("profileCard.contact")}
-            avatarUrl={avatarImg}
-            showUserInfo
-            enableTilt={true}
-            enableMobileTilt
-            onContactClick={() => {
-              window.location.href = 'mailto:marosvaridaniel7@gmail.com';
-            }}
-            behindGlowColor="rgba(125, 190, 255, 0.67)"
-            iconUrl={iconPatternImg}
-            behindGlowEnabled
-            innerGradient="linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)"
+      <div className="page-upper">
+        <div className="lightning-bg">
+          <Lightning
+            hue={260}
+            xOffset={0}
+            speed={1}
+            intensity={1}
+            size={1}
           />
+        </div>
+        <main className="hero-section">
+          <TextType
+            as="h1"
+            text={[t("hero.greeting"), t("hero.look"), t("hero.description")]}
+            typingSpeed={75}
+            pauseDuration={2500}
+            showCursor
+            cursorCharacter="_"
+            deletingSpeed={50}
+            variableSpeed={{ min: 60, max: 120 }}
+            cursorBlinkDuration={0.5}
+          /></main>
+
+        <PillNav
+          logo={logo}
+          logoAlt="Company Logo"
+          items={[
+            { label: t("nav.main"), href: 'MarosvariDanielCV/' },
+            { label: t("nav.time"), href: 'MarosvariDanielCV/timeline' },
+            { label: t("nav.studies"), href: 'MarosvariDanielCV/studies' },
+            { label: t("nav.projects"), href: 'MarosvariDanielCV/projects' }
+          ]}
+          activeHref="/"
+          className="custom-nav"
+          ease="power2.easeOut"
+          baseColor="#000000"
+          pillColor="#ffffff"
+          hoveredPillTextColor="#ffffff"
+          pillTextColor="#000000"
+          initialLoadAnimation
+        />
+
+        <div className="profile-card-outer">
+          <div className="profile-card-inner">
+            <ProfileCard
+              name="Marosvári Dániel"
+              title={t("profileCard.job")}
+              handle="SirLancelot002"
+              status={t("profileCard.online")}
+              contactText={t("profileCard.contact")}
+              avatarUrl={avatarImg}
+              showUserInfo
+              enableTilt={true}
+              enableMobileTilt
+              onContactClick={() => {
+                window.location.href = 'mailto:marosvaridaniel7@gmail.com';
+              }}
+              behindGlowColor="rgba(125, 190, 255, 0.67)"
+              iconUrl={iconPatternImg}
+              behindGlowEnabled
+              innerGradient="linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)"
+            />
+          </div>
         </div>
       </div>
       <div className="container mt-5">
